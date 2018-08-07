2749904f7592272047d1d94225f3aab754a1ab16
Paul Campbell <paul@rslw.com>
2018-08-07 12:14:22 +0100

    Adds callback_url method to fix omniauth/omniauth-oauth2#81

--- lib/omniauth-tito.rb
+++ lib/omniauth-tito.rb
@@ -23,6 +23,10 @@ module OmniAuth
       def raw_info
         @raw_info ||= access_token.get('/me.json').parsed
       end
+
+      def callback_url
+        full_host + script_name + callback_path
+      end
     end
   end
 end
